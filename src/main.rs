240069128c6164366bec4b2c78bca20cd99e264c
Alden Davidson <adavidson+github@protonmail.ch>
2022-01-11 14:47:27 -0800

    game now loops rather than displaying once, and can accept keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::time::Duration;
+
+use crossterm::event::{poll, read, Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use sudoku::Sudoku;
 use tui::backend::CrosstermBackend;
 use tui::layout::{Constraint, Rect};
-use tui::widgets::{Block, Borders, Row, Table};
+use tui::widgets::{Block, Borders, Cell, Row, Table, TableState};
 use tui::Terminal;
 mod sudoku;
 
@@ -9,7 +13,7 @@ fn main() -> Result<(), std::io::Error> {
     let stdout = std::io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    terminal.clear()?;
+    enable_raw_mode()?;
 
     let sudoku_raw = vec![
         [5, 3, 0, 6, 0, 0, 0, 9, 8],
@@ -24,43 +28,66 @@ fn main() -> Result<(), std::io::Error> {
     ];
     let sudoku_boxes = Sudoku::from(sudoku_raw);
 
-    let mut sudoku_rows = Vec::new();
-    for index in 0..9 {
-        if let Some(el_row) = sudoku_boxes.row(index) {
-            let row_str: Vec<String> = el_row.iter().map(|el| el.to_string()).collect();
-            let mut row = Row::new(row_str);
-            if index % 3 == 2 && index != 8 {
-                row = row.bottom_margin(1);
+    let mut table_state = TableState::default();
+
+    terminal.clear()?;
+    loop {
+        let mut sudoku_rows = Vec::new();
+        for index in 0..9 {
+            if let Some(el_row) = sudoku_boxes.row(index) {
+                let row_cells: Vec<Cell> =
+                    el_row.iter().map(|el| Cell::from(el.to_string())).collect();
+                let mut row = Row::new(row_cells);
+                if index % 3 == 2 && index != 8 {
+                    row = row.bottom_margin(1);
+                }
+                sudoku_rows.push(row);
             }
-            sudoku_rows.push(row);
+        }
+
+        let table = Table::new(sudoku_rows)
+            .block(Block::default().title("Sudoku").borders(Borders::ALL))
+            .widths(&[
+                Constraint::Length(2),
+                Constraint::Length(2),
+                Constraint::Length(3),
+                Constraint::Length(2),
+                Constraint::Length(2),
+                Constraint::Length(3),
+                Constraint::Length(2),
+                Constraint::Length(2),
+                Constraint::Length(2),
+            ])
+            .column_spacing(1);
+
+        let width = 3 * 7 + 2 * 3 + 2;
+        let height = 3 * 3 + 2 + 2;
+
+        terminal.draw(|f| {
+            // let size = f.size();
+            let size = Rect {
+                x: 0,
+                y: 0,
+                width,
+                height,
+            };
+            f.render_widget(table, size);
+        })?;
+
+        if poll(Duration::from_secs(0))? {
+            match read()? {
+                Event::Key(event) => match event.code {
+                    KeyCode::Char('q') | KeyCode::Esc => {
+                        disable_raw_mode()?;
+                        break;
+                    }
+                    other => println!("\r\n{:?}", other),
+                },
+                Event::Mouse(event) => println!("{:?}", event),
+                Event::Resize(width, height) => println!("New size {}x{}", width, height),
+            }
         }
     }
 
-    let table = Table::new(sudoku_rows)
-        .block(Block::default().title("Sudoku").borders(Borders::ALL))
-        .widths(&[
-            Constraint::Length(2),
-            Constraint::Length(2),
-            Constraint::Length(3),
-            Constraint::Length(2),
-            Constraint::Length(2),
-            Constraint::Length(3),
-            Constraint::Length(2),
-            Constraint::Length(2),
-            Constraint::Length(2),
-        ])
-        .column_spacing(1);
-
-    terminal.draw(|f| {
-        let size = f.size();
-        let size = Rect {
-            x: 0,
-            y: 0,
-            width: 29,
-            height: 13,
-        };
-        f.render_widget(table, size);
-    })?;
-    println!("");
     Ok(())
 }
